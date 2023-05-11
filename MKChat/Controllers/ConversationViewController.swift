2f6820730f413dc57cbde88ceaf27998eeca8f1f
Michał Wolanin <michalwolanin@MacBook-Pro-Micha.local>
2023-05-11 22:47:13 +0200

    Starting project, Setup Log In, Register Screens

---
+++ MKChat/Controllers/ConversationViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  MKChat
+//
+//  Created by Michał Wolanin on 10/05/2023.
+//
+
+import UIKit
+
+class ConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+        
+        
+        
+    }
+    
+
+    
+}
+
+
+
+
+
+
+
+
---
+++ MKChat/Resources/Extensions.swift
@@ -0,0 +1,45 @@
+//
+//  Extensions.swift
+//  MKChat
+//
+//  Created by Michał Wolanin on 11/05/2023.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIView {
+    
+    
+    public var width: CGFloat{
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat{
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat{
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat{
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat{
+        return self.frame.origin.x
+    }
+    
+    public var rigth: CGFloat{
+        return self.frame.size.width + self.frame.origin.x
+    }
+    
+}
+
+
+
+
+
+
